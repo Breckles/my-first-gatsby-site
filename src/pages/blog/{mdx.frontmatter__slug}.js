318c89f3b8c4a38261ae51b3d24c8c9e230b3890
Waylon Carrier Nunes <wayloncn@gmail.com>
2024-05-28 14:54:25 -0400

    Finishes Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 
 const BlogPost = ({ data, children }) => {
+  // getImage(data.mdx.frontmatter.hero_image) == data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle="Super Cool Blog Posts">
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{' '}
+        <a href={data.mdx.frontmatter.hero_image_credit_link} target="blank">
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   );
@@ -19,6 +30,14 @@ export const query = graphql`
       frontmatter {
         date(formatString: "MMMM D, YYYY")
         title
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
